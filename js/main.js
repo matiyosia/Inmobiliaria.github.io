4d44316f790a7893d218eeab4e9d0938bf471148
Matias Yosia <93013680+matiyosia@users.noreply.github.com>
2021-10-24 21:08:05 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const $navegador = document.getElementById("nav");
+const $btnMenu = document.getElementById("btn-menu");
+
+
+$btnMenu.addEventListener("click", ()=>{
+
+    $navegador.classList.toggle("abre")
+
+    
+});
+
+
